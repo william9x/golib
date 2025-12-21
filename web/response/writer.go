7d80d8535051de9720032235c94a8d09951f41cb
LiamH <lehoangnam197@gmail.com>
2025-12-21 23:15:41 +0700

    feat: Add response Write2 without data & meta

--- web/response/writer.go
+++ web/response/writer.go
@@ -9,6 +9,11 @@ func Write(ctx *fiber.Ctx, res Response) error {
 	return render.Render(ctx, res.Meta.HttpStatus(), render.JSON{Data: res})
 }
 
+// Write2 is an alias of Write, without data & meta in response body
+func Write2(ctx *fiber.Ctx, res Response) error {
+	return render.Render(ctx, res.Meta.HttpStatus(), render.JSON{Data: res.Data})
+}
+
 func WriteError(ctx *fiber.Ctx, err error) error {
 	return Write(ctx, Error(err))
 }
